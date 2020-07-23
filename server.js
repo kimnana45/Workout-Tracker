dc4bbf15e2ad43e1125400c4ca0b08da156c65af
kimnana45 <kimle.lek@gmail.com>
2020-07-23 15:59:39 -0400

    added models/ workout schema/set up server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+const db = require("./models");
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+// routes
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}!`));
